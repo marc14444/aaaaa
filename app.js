a1ae3227fbac2704c607763f1cad64010786554b
Marc Harden <Marc_Harden@192.168.50.17>
2023-09-08 12:10:18 +0000

    deuxieme commite

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ btn.addEventListener('click', evenement);
     console.log(e.key)
 }) */
 
-function snake_case(chaine){
+/* function snake_case(chaine){
     if(typeof chaine === "string"){
         const miniscules = 'abcdefghijklmnopqrstuvwyz';
         const majuscules = miniscules.toUpperCase();
@@ -181,4 +181,54 @@ function snake_case(chaine){
     }
     return "la chaine n'est pas en chaine";
 }
-console.log(snake_case("bonjourMonsieur"));
+console.log(snake_case("bonjourMonsieur")); */
+
+
+function min(tableau) {
+    if (
+      Array.isArray(tableau) &&
+      tableau.every(function (element) {
+        return typeof element === 'number';
+      }) &&
+      tableau.length > 0
+    ) {
+      let min = tableau[0];
+      for (const nombre of tableau) {
+        if (nombre < min) {
+          min = nombre;
+        }
+      }
+      return min;
+    }
+  
+    return -1;
+  }
+  
+  function max(tableau) {
+    if (
+      Array.isArray(tableau) &&
+      tableau.every(function (element) {
+        return typeof element === 'number';
+      }) &&
+      tableau.length > 0
+    ) {
+      let max = tableau[0];
+      for (const nombre of tableau) {
+        if (nombre > min) {
+          max = nombre;
+        }
+      }
+      return max;
+    }
+  
+    return -1;
+  }
+  
+  function min_max(tableau) {
+    return [min(tableau), max(tableau)];
+  }
+  
+  const arr1 = ['kjfjkdkjs', -1000, 2, -100];
+  
+  console.log(min(arr1), max(arr1));
+  console.log(min_max(arr1));
